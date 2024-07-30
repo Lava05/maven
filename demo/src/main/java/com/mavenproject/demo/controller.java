ad8561ccc969ee571c0d84c4455df3c71c6b9896
Lava05 <168163992+Lava05@users.noreply.github.com>
2024-07-30 15:01:18 +0530

    Add files via upload

---
+++ demo/src/main/java/com/mavenproject/demo/controller.java
@@ -0,0 +1,16 @@
+package com.mavenproject.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class controller {
+
+    @GetMapping("/")
+    public String getMessage() {
+        return "Hello World";
+    }
+}
